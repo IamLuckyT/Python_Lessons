e03ba38b35526c7c1f0109bf58de3cb32c81d340
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-08-09 09:13:19 +0200

    Create whileloop.py

---
+++ whileloop.py
@@ -0,0 +1,7 @@
+count = 0
+
+while count < 5:
+  print(count)
+  count += 1            #Increments(adds) by 1 on every iteration i.e 0 + 1 = 1, 1 + 1 = 2 and so om
+  if count == 3
+    break               #Breaks and stops the loop when it reaches 3
