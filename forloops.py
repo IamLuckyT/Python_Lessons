f4d70f0fbee54a08734e5e04c8910920201b447c
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-08-09 09:07:26 +0200

    Update forloops.py

--- forloops.py
+++ forloops.py
@@ -1,1 +1,25 @@
+grocery = ["2Kg Meat", "Pap", "Rice", "Spaghetti", "Mayo", "Tomato Sauce", "Wors", "Minced Meat", "Spices", "Soup", "Tin fish", "Baked Beans"]
+
+for item in grocery:
+  print(item)
+
+print()                     #Create a gap by printing an empty statement
+for item in grocery:
+  if item == Rice:
+    break                   #Closes loop if Rice is found
+    print(item)
+
+print()  
+
+for item in grocery:
+  if item == Rice:
+    continue                #Skips the Rice and moves to the next item which is Spaghetti
+    print(item)
+
+print()  
+
+for item in grocery:
+  if item == Rice:
+    pass                    #Doesnt do anything to Rice and prints items as is
+    print(item)
 
