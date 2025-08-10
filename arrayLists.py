f3e07bd2dac9e8f54fc591b29ba10d803006b4b1
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-08-10 22:43:33 +0200

    Added more functions which are append, insert, remove and sort to the fruits array.

--- arrayLists.py
+++ arrayLists.py
@@ -2,6 +2,13 @@ grocery = ["2Kg Meat", "Pap", "Rice", "Spaghetti", "Mayo", "Tomato Sauce", "Wors
 fruits = ["Apple", "Banana", "Orange", "Grapes", "Mango", "Pineapple", "Strawberry"]
 vegetables = ["Carrot", "Green Pepper", "Spinach", "Potato", "Cabbage", "Onion", "Garlic"]
 
+fruits.append("Tomato")                     #Adds an item in the array
+fruits.insert(1, "Watermelon")              #Adds watermelon on the 1st part of the items
+fruits.remove("Tomato")                     #Removes tomato from the list
+fruits.sort()                               #Sorts the items in alphabetical order
+fruits.reverse()                            #Reverses the order of the items
+
+
 userName = input("Enter your name: ")
 
 print("Hey " + userName + ", from the grocery list, which items will you like to buy?")
