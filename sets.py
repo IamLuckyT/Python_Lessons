91de94185a0035bae0b2d3e302c375514f2787f3
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-08-12 22:26:58 +0200

    Update sets.py

--- sets.py
+++ sets.py
@@ -1,4 +1,4 @@
-#Sets 
+#Sets are used when you have a collection of unique elements
 
 my_set = {1, 2, 3, 4, 5}
 print(my_set}
@@ -10,3 +10,22 @@ print(my_set)
 print()
 my_set.remove(3)
 print(my_set)
+
+print()
+set1 = {1, 2, 3, 4}
+set2 = {4, 5, 6}
+
+#Union of sets
+union_set = set1.union(set2)
+print(union_set)
+print()
+
+#Intersection of sets
+int_sets = set1.intersection(set2)
+print(int_sets)
+print()
+
+#difference between sets
+diff_sets = set1.difference(set2)
+print(diff_sets)
+print()
