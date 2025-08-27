7fd1a6e8b86e6b149ca3150fbb97b88d0ce20424
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-08-28 00:00:07 +0200

    Update implement.py

--- implement.py
+++ implement.py
@@ -1,3 +1,11 @@
 #All the code for the lessons will be implemented here
 
+grocery = ["2Kg Meat", "Pap", "Rice", "Spaghetti", "Mayo", "Tomato Sauce", "Wors", "Minced Meat", "Spices", "Soup", "Tin fish", "Baked Beans"]
+fruits = ["Apple", "Banana", "Orange", "Grapes", "Mango", "Pineapple", "Strawberry"]
+vegetables = ["Carrot", "Green Pepper", "Spinach", "Potato", "Cabbage", "Onion", "Garlic"]
+
+userName = input("Enter your name: ")
+
+print("Hey " + userName + ", from the grocery list, which items will you like to buy?")
+print("You can choose from the following items:")
 
