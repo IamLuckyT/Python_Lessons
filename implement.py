01ebf18e00394be2ea0bd61fce9d2ee2a4751e66
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-08-28 10:48:13 +0200

    Added more functions to handle different input types

--- implement.py
+++ implement.py
@@ -8,4 +8,14 @@ userName = input("Enter your name: ")
 
 print("Hey " + userName + ", from the grocery list, which items will you like to buy?")
 print("You can choose from the following items:")
+for fruit in fruits:
+    print(fruit)
+
+user_choice = input("Which fruit do you like? ")
+
+if user_choice in fruits:
+    print(f"You chose: {user_choice}")
+else:
+    print("Sorry, that fruit is not in the list.")
+
 
