ec2a44b609bcce03f7e7b0b08a5c52fa10a05392
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-08-28 15:43:51 +0000

    Created the groceries dictionary and an output for them

---
+++ dictionaries.py
@@ -0,0 +1,15 @@
+grocery = {'2Kg Meat': 250,
+            'Pap': 150, 
+            'Rice': 120,
+            'Spaghetti': 15,
+            'Mayo': 30,
+            'Tomato Sauce': 32,
+            'Wors': 80,
+            'Minced Meat': 25,
+            'Spices': 6,
+            'Soup': 5,
+            'Tin fish': 15,
+            'Baked Beans': 18
+}
+for items in grocery:
+    print(items)
