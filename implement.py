94796d5a0dd708f2c9cd58dc3662c0a43b7b074c
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-09-03 13:33:48 +0000

    Made sure that even if the user enters different selection ie Tin Fish instead of Tin fish it doesnt fall on the invalid choices

--- implement.py
+++ implement.py
@@ -1,19 +1,23 @@
 #All the code for the lessons will be implemented here
 
 grocery = ["2Kg Meat", "Pap", "Rice", "Spaghetti", "Mayo", "Tomato Sauce", "Wors", "Minced Meat", "Spices", "Soup", "Tin fish", "Baked Beans"]
-fruits = ["Apple", "Banana", "Orange", "Grapes", "Mango", "Pineapple", "Strawberry"]
-vegetables = ["Carrot", "Green Pepper", "Spinach", "Potato", "Cabbage", "Onion", "Garlic"]
+fruits = ["Apples", "Bananas", "Oranges", "Grapes", "Mango", "Pineapples", "Strawberry"]
+vegetables = ["Carrots", "Green Peppers", "Spinach", "Potatoes", "Cabbages", "Onions", "Garlic"]
 
 userName = input("Enter your name: ")
 
 print("Hey " + userName + ", from the grocery list which items will you like to buy?")
+print()
+
 print("You can choose from the following grocery items:")
 for item in grocery:
     print(item)
 
 print()
 
-print("NB : Use a comma to separate items")
+print("NB : USE A COMMA TO SEPARATE YOUR CHOICES")
+
+print()
 
 user_choice = input("Select the item/items would you like to buy: ")
 
@@ -24,11 +28,12 @@ else:
     print("Sorry, that fruit is not in the list.")
 '''
 # Split input by commas and strip whitespace
-chosen_items = [item.strip() for item in user_choice.split(",")]
 
-# Check which chosen fruits are valid
-valid_choices = [item for item in chosen_items if item in grocery]
-invalid_choices = [item for item in chosen_items if item not in grocery]
+# Make comparison case-insensitive
+grocery_lower = [item.lower() for item in grocery]
+chosen_items = [item.strip() for item in user_choice.split(",")]
+valid_choices = [grocery[grocery_lower.index(item.lower())] for item in chosen_items if item.lower() in grocery_lower]
+invalid_choices = [item for item in chosen_items if item.lower() not in grocery_lower]
 
 print(f"The items you want to buy are: {valid_choices}")
 
@@ -49,8 +54,11 @@ user_choice = input("Select fruits you would like to buy: ")
 
 chosen_items = [item.strip() for item in user_choice.split(",")]
 
-valid_choices = [fruit for fruit in chosen_items if fruit in fruits]
-invalid_choices = [fruit for fruit in chosen_items if fruit not in fruits]
+
+# Make comparison case-insensitive for fruits
+fruits_lower = [fruit.lower() for fruit in fruits]
+valid_choices = [fruits[fruits_lower.index(fruit.lower())] for fruit in chosen_items if fruit.lower() in fruits_lower]
+invalid_choices = [fruit for fruit in chosen_items if fruit.lower() not in fruits_lower]
 
 print(f"The fruits you want to buy are: {valid_choices}")
 
